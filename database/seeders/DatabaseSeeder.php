aa26dbff46cc1baca24f512118f91cd7bfcb33e6
oshu <99813212+oshudev@users.noreply.github.com>
2025-02-07 12:48:21 +0800

    Add db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Models\Store;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 final class DatabaseSeeder extends Seeder
@@ -15,11 +17,20 @@ final class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john.doe@example.com',
+            'password' => bcrypt('secret'),
+            'email_verified_at' => Carbon::now(),
         ]);
+
+        $store = Store::create([
+            'name' => 'Acme',
+            'slug' => 'acme',
+            'user_id' => $user->id,
+        ]);
+
+        $user->store_id = $store->id;
+        $user->save();
     }
 }
